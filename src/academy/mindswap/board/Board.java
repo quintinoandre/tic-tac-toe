cdc67f4a6a5061bd6305f2f726edb5abf28163a9
Tânia <tania.silva@mindswap.academy>
2022-08-20 15:36:42 +0100

    feat(board): create board class

---
+++ src/academy/mindswap/board/Board.java
@@ -0,0 +1,110 @@
+package academy.mindswap.board;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Board implements ActionListener {
+    JFrame frame = new JFrame();
+    JPanel boardTitle = new JPanel(); //contém o título
+    JPanel boardButtons = new JPanel(); // contém os butões
+    JLabel textField = new JLabel(); //para display uma mensagem qualquer
+    JButton[] buttons = new JButton[9];
+    private final int BOARD_HEIGHT = 800;
+    private final int BOARD_WIDTH = 800;
+    private final int TITLE_HEIGHT = 200;
+    private final int TITLE_WIDTH = 800;
+    private String playerMove;
+    private char[][] gameState;
+
+    private void createBoard() {
+
+        //gameIntro();
+        boardFrame();
+        boardTitle();
+        buttonsGrid();
+    }
+
+    private void boardFrame() {
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(BOARD_WIDTH, BOARD_HEIGHT);
+        frame.getContentPane().setBackground(new Color(219, 219, 219));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+    }
+
+    /*private void gameIntro(){
+        textField.setBackground(Color.BLACK);
+        textField.setForeground(Color.BLACK);
+        textField.setFont(new Font("Monospaced", Font.BOLD, 40));
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
+        textField.setText("TIC TAC TOE on steroids");
+        textField.setOpaque(true);
+
+        try{
+            Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+     */
+
+    private void boardTitle() {
+        textField.setBackground(Color.BLACK);
+        textField.setForeground(new Color(219, 219, 219));
+        textField.setFont(new Font("Monospaced", Font.BOLD, 40));
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
+        textField.setText("TIC TAC TOE CHALLENGE");
+        textField.setOpaque(true);
+
+        //o text field anterior vai estar displayed neste titlePanel
+        boardTitle.setLayout(new BorderLayout());
+        boardTitle.setBounds(0, 0, TITLE_WIDTH, TITLE_HEIGHT);
+
+        boardTitle.add(textField);
+        frame.add(boardTitle, BorderLayout.NORTH);
+    }
+
+    private void buttonsGrid() {
+        boardButtons.setLayout(new GridLayout(3, 3));
+        boardButtons.setBackground(new Color(150, 150, 150));
+        frame.add(boardButtons);
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            boardButtons.add(buttons[i]);
+            buttons[i].setOpaque(true);
+            buttons[i].setFont(new Font("Monospaced", Font.BOLD, 50));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == buttons[i]) {
+
+            }
+        }
+    }
+
+    public String getPlayerMove() {
+        return playerMove;
+    }
+
+    public void setPlayerMove(String playerMove) {
+        this.playerMove = playerMove;
+    }
+
+    public void setGameState(char[][] gameState) {
+        this.gameState = gameState;
+    }
+}
+
+
