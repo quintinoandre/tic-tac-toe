a515d23ba4cbbf2edbeae6d0fe6e1f44c4a45deb
Filipe Furtado <filipe.furtado@mindswap.academy>
2022-08-21 11:07:53 +0100

    feat(game): create game messages class

---
+++ src/academy/mindswap/game/GameMessages.java
@@ -0,0 +1,13 @@
+package academy.mindswap.game;
+
+public final class GameMessages {
+
+
+    private GameMessages() {
+    }
+
+    public static final String PLAYER_SOCKET_CLOSED = "Player socket is already closed";
+    public static final String PLAYER_LEFT = "Player left the game!";
+    public static final String WELCOME_MESSAGE = "Welcome, %s, to the Tic Tac Toe game!";
+
+}
